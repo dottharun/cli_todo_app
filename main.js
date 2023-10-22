067a31d21b860ac213efcaa9a0cb6bc69327e8bb
ubutharun <tharun242424@gmail.com>
2023-10-22 16:16:19 +0530

    db connection working with insertion

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 const { argv } = require("process");
 const assert = require("assert");
-
-//db
 const { Client } = require("pg");
+require("dotenv").config();
 
 argv.splice(0, 2);
 assert(
   argv.length == 1 || argv.length == 2,
   "no of input args must be less than 2"
 );
+
+//db
+const client = new Client({
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+});
+console.log(`client made`);
 
 const logAllOptions = () => {
   console.log(`Options:
@@ -25,37 +34,54 @@ const logAppVersion = () => {
   console.log(ver);
 };
 
-switch (argv[0]) {
-  case "--new":
-    console.log(`inserting item ${argv[1]}`);
-    insertItem(argv[1]);
-    break;
-  case "--list":
-    console.log(`listing todos acc to format - ${argv[1]}`);
-    //may need to check for format
-    logTodosAccToFormat(argv[1]);
-    break;
-  case "--done":
-    console.log(`updating a todo item as done - id: ${argv[1]}`);
-    updateTodoAsDone(argv[1]);
-    break;
-  case "--delete":
-    console.log(`deleting a todo item -id: ${argv[1]}`);
-    deleteTodo(argv[1]);
-    break;
-  case "--help":
-    logAllOptions();
-    break;
-  case "--version":
-    logAppVersion();
-    break;
-  default:
-    console.log(`Pls enter a valid command - not doing anything`);
-}
+//   db funcs
+const insertItem = async (todoItem) => {
+  await client.connect();
+  console.log(`client connected`);
 
-// --new to add a new todo item
-// --list [all|pending|done] to list the todo items
-// --done [id] to update a todo item
-// --delete [id] to delete a todo item
-// --help to list all the available options
-// --version to pr
+  try {
+    const res = await client.query(
+      `INSERT INTO todos (todo, status) VALUES ($1, $2);`,
+      [todoItem, "pending"]
+    );
+    console.log(`added the todo - ${todoItem}`);
+  } catch (e) {
+    console.log("db insertion error", e);
+  } finally {
+    await client.end();
+  }
+};
+
+const init = async () => {
+  console.log(`init started`);
+
+  switch (argv[0]) {
+    case "--new":
+      console.log(`inserting item ${argv[1]}`);
+      await insertItem(argv[1]);
+      break;
+    case "--list":
+      console.log(`listing todos acc to format - ${argv[1]}`);
+      //may need to check for format
+      logTodosAccToFormat(argv[1]);
+      break;
+    case "--done":
+      console.log(`updating a todo item as done - id: ${argv[1]}`);
+      updateTodoAsDone(argv[1]);
+      break;
+    case "--delete":
+      console.log(`deleting a todo item -id: ${argv[1]}`);
+      deleteTodo(argv[1]);
+      break;
+    case "--help":
+      logAllOptions();
+      break;
+    case "--version":
+      logAppVersion();
+      break;
+    default:
+      console.log(`Pls enter a valid command - not doing anything`);
+  }
+};
+
+init();
